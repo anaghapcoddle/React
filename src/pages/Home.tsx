c16e1575f80148dbfeb842d1d6610358a8f0c50e
Anagha P <anagha.p@coddletech.com>
2023-12-12 11:00:28 +0530

    completed header and footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
+import './Home.css';
 
 function Home() {
   return (
-    <>
+    <div className="home-container">
       <Header />
-      <div>
+      <div className="home-content">
         <h1>Welcome to the home page.</h1>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
