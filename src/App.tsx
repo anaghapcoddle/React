2908fc5d8275b62dae71024e49cfe8fc245c413a
Anagha P <anagha.p@coddletech.com>
2023-12-19 02:11:14 +0530

    add order page in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,12 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import {
-  useNavigate,
-  useLocation,
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import { getData } from './utils/apiUtils';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './utils/privateRoute';
 
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Table from './components/Table';
-
-interface PrivateRouteProps {
-  element: React.ReactNode;
-}
-
-function PrivateRoute({ element }: PrivateRouteProps) {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    async function verifyToken() {
-      const token = localStorage.getItem('token');
-
-      if (!token && location.pathname !== '/login') {
-        navigate('/login');
-      }
-      const res = await getData(`${process.env.REACT_APP_API_URL}/menu/view`);
-      if (res.error !== null) {
-        navigate('/login');
-      }
-    }
-    verifyToken();
-  }, [navigate, location.pathname]);
-
-  const storedToken = localStorage.getItem('token');
-  return storedToken ? (element as JSX.Element) : null;
-}
 
 function App() {
   useEffect(() => {
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,15 +4,23 @@ import Layout from './Layout';
 import './Table.css';
 import { getData } from '../utils/apiUtils';
 
+interface MenuItem {
+  id: number;
+  name: string;
+  quantity: number;
+}
+
 function Table() {
-  const { tableId } = useParams();
-  const [menuItems, setMenuItems] = useState<any[]>([]);
+  const { tableId } = useParams<{ tableId: string }>();
+  const [menuItems, setMenuItems] = useState<MenuItem[]>([]);
+  const [option, setOption] = useState<number | undefined>();
+  const [itemList, settemList] = useState<MenuItem[]>([]);
 
   useEffect(() => {
     async function GetMenuData() {
       const res = await getData(`${process.env.REACT_APP_API_URL}/menu/view`);
       const { data } = res.data;
-      const menuItemsWithQuantity = data.map((item: any) => ({
+      const menuItemsWithQuantity: MenuItem[] = data.map((item: MenuItem) => ({
         ...item,
         quantity: 0,
       }));
@@ -21,22 +29,31 @@ function Table() {
     GetMenuData();
   }, []);
 
-  const handleIncrement = (itemId: string) => {
-    setMenuItems((prevMenuItems) =>
-      prevMenuItems.map((item) =>
-        item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
-      )
+  function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const value = parseInt(event.target.value, 10);
+    setOption(value);
+  }
+
+  const handleIncrement = (itemId: number) => {
+    const updatedMenuItems = menuItems.map((item) =>
+      item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
     );
+    setMenuItems(updatedMenuItems);
   };
 
-  const handleDecrement = (itemId: string) => {
-    setMenuItems((prevMenuItems) =>
-      prevMenuItems.map((item) =>
-        item.id === itemId && item.quantity > 0
-          ? { ...item, quantity: item.quantity - 1 }
-          : item
-      )
+  const handleDecrement = (itemId: number) => {
+    const updatedMenuItems = menuItems.map((item) =>
+      item.id === itemId && item.quantity > 0
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
     );
+    setMenuItems(updatedMenuItems);
+  };
+
+  const addToArray = (selectedItem: MenuItem | undefined) => {
+    if (selectedItem) {
+      settemList((current) => [...current, selectedItem]);
+    }
   };
 
   return (
@@ -46,50 +63,64 @@ function Table() {
           <div className="heading">
             <span>Order for Table {tableId}</span>
           </div>
-          <div className="menu-cart-wrapper">
-            <div className="menu-items-container">
-              <ul>
+          <div className="order-items-wrapper">
+            <div className="order-items-item-heading">
+              <span>Item</span>
+            </div>
+            <div className="order-items-quantity-heading">
+              <span>Quantity</span>
+            </div>
+            <div />
+            <div className="select-item-wrapper">
+              <select name="option" onChange={handleChange}>
+                <option value="Select an item">Select an item</option>
                 {menuItems.map((menuItem) => (
-                  <li key={menuItem.id} className="menu-item">
-                    <span>{menuItem.name}</span>
-                    <div className="quantity">
-                      <button
-                        type="button"
-                        className="decrement-button quantity-element"
-                        onClick={() => handleDecrement(menuItem.id)}
-                      >
-                        -
-                      </button>
-                      <span className="quantity-container quantity-element">
-                        {menuItem.quantity}
-                      </span>
-                      <button
-                        type="button"
-                        className="increment-button quantity-element"
-                        onClick={() => handleIncrement(menuItem.id)}
-                      >
-                        +
-                      </button>
-                    </div>
-                  </li>
+                  <option value={menuItem.id}>{menuItem.name}</option>
                 ))}
-              </ul>
+              </select>
             </div>
-            <div className="cart">
-              <span className="cart-heading">Ordered Items</span>
-              <div className="cart-body">
-                {menuItems
-                  .filter((menuItem) => menuItem.quantity > 0)
-                  .map((menuItem) => (
-                    <div key={menuItem.id} className="cart-item">
-                      <span>{menuItem.name}</span>
-                      <span>{menuItem.quantity}</span>
-                    </div>
-                  ))}
-                <button type="button">Confirm Order</button>
-              </div>
+            <div className="quantity">
+              <button
+                type="button"
+                className="decrement-button quantity-element"
+                onClick={() => {
+                  if (option !== undefined) {
+                    handleDecrement(menuItems[option - 1].id);
+                  }
+                }}
+              >
+                -
+              </button>
+              <span className="quantity-container quantity-element">
+                {option !== null &&
+                  menuItems.find((item) => item.id === option)?.quantity}
+              </span>
+              <button
+                type="button"
+                className="increment-button quantity-element"
+                onClick={() => {
+                  if (option !== undefined) {
+                    handleIncrement(menuItems[option - 1].id);
+                  }
+                }}
+              >
+                +
+              </button>
             </div>
+            <button
+              type="button"
+              onClick={() => {
+                if (option !== undefined) {
+                  addToArray(menuItems[option - 1]);
+                }
+              }}
+            >
+              Add Item
+            </button>
           </div>
+          {itemList.map((item) => (
+            <p key={item.id}>{item.name}</p>
+          ))}
         </div>
       </Layout>
     </div>
